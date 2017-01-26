083d6610cccf2521b317897e7986a993a360847d
Jiri Dolezal <dolezji6@fel.cvut.cz>
2017-01-26 04:27:38 +0100

    bugfix2

--- app/src/main/java/android/jirix/cz/wifiscanner/WifiConnectionFragment.java
+++ app/src/main/java/android/jirix/cz/wifiscanner/WifiConnectionFragment.java
@@ -58,7 +58,7 @@ public static WifiConnectionFragment create(){
    }
 
    private void setVisibility(){
-      mRootView.setVisibility(mWifiManager.isWifiEnabled() ? View.VISIBLE : View.GONE);
+      mRootView.setVisibility(mWifiManager.isWifiEnabled() ? View.VISIBLE : View.INVISIBLE);
    }
 
    @Override
